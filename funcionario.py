fcbe12c57d5294700f08fefffbe19aa0fcc6a49c
Felipe Ramos <felipe.pi@outlook.com>
2018-06-23 14:54:08 -0300

    Aula 11

---
+++ funcionario.py
@@ -0,0 +1,13 @@
+class Funcionario(Pessoa):
+    matricula = ""
+    cargo = ""
+    
+    def __init__(self, nome, matricula):
+        pass
+
+    def __init__ super(Pessoa):
+        pass
+
+    def trabalha():
+        print("Funcionario trabalhando...")
+
---
+++ new_funcionario.py
@@ -0,0 +1,7 @@
+from funcionario import Funcionario 
+
+funcionario1 = Funcionario(nome = "Felipe", matricula = "6465")
+
+funcionario1.idade  = 29
+print(funcionario1.matricula)
+funcionario1.trabalha()
