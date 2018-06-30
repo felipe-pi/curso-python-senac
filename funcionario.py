d923d92b930d52bce0e551b94cddedfb049798bf
Felipe Ramos <felipe.pi@outlook.com>
2018-06-30 14:35:59 -0300

    Aula11

--- funcionario.py
+++ funcionario.py
@@ -1,6 +1,9 @@
+from pessoa import Pessoa 
+
 class Funcionario(Pessoa):
     matricula = ""
     cargo = ""
+    atividade = ""
     
     def __init__(self, nome, matricula):
         pass
@@ -8,6 +11,11 @@ class Funcionario(Pessoa):
     # def __init__ super(Pessoa):
     #     pass
 
-    def trabalha():
-        print("Funcionario trabalhando...")
+    def trabalha(self, atividade):
+        if atividade == 1:
+            return print("Funcionario trabalhando...")
+        elif atividade == 0:
+            return print("Funcionário fora do expediente...")
+        else:
+            print("Error")
 
